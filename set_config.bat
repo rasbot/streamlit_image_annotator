ac8bb52352c212c37dec81cbeb3d1e5772d53597
rasbot <nathan.f.rasmussen@gmail.com>
2023-02-26 12:33:03 -0800

    remove json stuff

--- set_config.bat
+++ set_config.bat
@@ -1,6 +1,6 @@
 @echo off
 set /p default_directory=Please enter a default directory path: 
-set /p categories=Set default category buttons. See the README for more info. Hit enter to use defaults "keep, delete, fix, other" or provide a comma separated list here: 
+set /p default_categories=Set default category buttons. See the README for more info. Hit enter to use defaults "keep, delete, fix, other" or provide a comma separated list here: 
 set "streamlit_config=%USERPROFILE%\.streamlit\credentials.toml"
 
 REM Add do not track to streamlit credientials file
@@ -14,13 +14,12 @@ if %errorlevel% equ 0 (
   echo Lines added to "%streamlit_config%".
 )
 
-REM Replace the directory value in the YAML file
+REM Replace the directory value and category string in the YAML file
 setlocal enableDelayedExpansion
 (for /f "tokens=1,* delims=:" %%a in (config.yml) do (
     set "line=%%b"
     if "%%a"=="default_directory" if not "%%b"=="" set "line= %default_directory%"
-    if "%%a"=="json_path" set "line= %default_directory%\annotations.json"
-    if "%%a"=="categories" set "line= %categories%"
+    if "%%a"=="default_categories" if not "%%b"=="" set "line= %default_categories%"
     echo %%a:!line!
 )) > config.tmp
 move /y config.tmp config.yml
